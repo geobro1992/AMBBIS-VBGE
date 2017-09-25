4e86977b056312a1252b23b9d4eaeaf5424c9b2d
geobro1992 <boa10gb@vt.edu>
2017-09-25 10:16:46 -0400

    tidy up code

--- PopDy_Project_Model.R
+++ PopDy_Project_Model.R
@@ -1,36 +1,28 @@
-setwd("C:/Users/boa10gb/Documents/R/VB Growth")
-
 #------------------------
 # Model 1 - Larval Growth
 #------------------------
 sink("PopDyProjLarva.txt")
 cat("
-    model{                                           
-    for (i in 1:N)     {
-    
+model{                                           
+  for (i in 1:N) {
     L[i] ~ dnorm(L_Exp[i], sig)   
-    L_Exp[i] <-  Linf*(1.0 - exp(-k*(A[i]-t0)))
+    L_Exp[i] <- Linf * (1.0 - exp(-k * (A[i] - t0)))
     A_ob[i] ~ dnorm(A[i], tau)
     A[i] ~ dunif(0.0001, 0.5)    
 
-
-    # posterior prediction
+# posterior prediction
     L.pred[i] ~ dnorm(L_Exp[i], sig)
     p.value[i] <- step(L.pred[i] - L[i])
-
-    
-    
   }
-    
-    Linf ~ dnorm(60, 0.0001)
-    k ~ dunif(0.01, 1)
-    t0 ~ dunif(-2, 2)
-    sig ~ dgamma(0.001, 0.0001)
-    tau ~ dgamma(0.001, 0.0001)
+
+# Priors  
+  Linf ~ dunif(30, 70)
+  k ~ dunif(0.01, 5)
+  t0 ~ dunif(-2, 2)
+  sig ~ dgamma(0.001, 0.0001)
+  tau ~ dgamma(0.001, 0.0001)
 }
-    
-    
-    ",fill=TRUE)
+  ", fill = TRUE)
 sink()
 
 
@@ -39,40 +31,34 @@ sink()
 #--------------------------------------------
 sink("PopDyProjLarvae.txt")
 cat("
-    model{                                           
-    for (i in 1:N)     {
-    
+model{                                           
+  for (i in 1:N) {
     L[i] ~ dnorm(L_Exp[i], sig)   
-    L_Exp[i] <-  Linf[i]*(1.0 - exp(-k[i]*(A[i]-t0)))
+    L_Exp[i] <-  Linf[i] * (1.0 - exp(-k[i] * (A[i] - t0)))
     A_ob[i] ~ dnorm(A[i], tau)
     A[i] ~ dunif(0.0001, 0.5)    
     
-    
-    # posterior prediction
+# posterior prediction
     L.pred[i] ~ dnorm(L_Exp[i], sig)
     p.value[i] <- step(L.pred[i] - L[i])
     
+    Linf[i] ~ dnorm(Linf_mu,  Linf_tau) I(20, 60)    
+    k[i] ~ dnorm(k_mu, k_tau) I(0.1, 5)
+  }
     
-     Linf[i] ~ dnorm(Linf_mu,  Linf_tau) I(20,60)    
-    k[i] ~ dnorm(k_mu, k_tau) I(0.1,5)
+  Linf_std <- sqrt(1 / Linf_tau)
+  k_std <- sqrt(1 / k_tau)
+  tau_std <- sqrt(1 / tau)
     
-    }
-    
-    Linf_std <- sqrt(1/Linf_tau)
-    k_std <- sqrt(1/k_tau)
-    tau_std <- sqrt(1/tau)
-    
-    Linf_mu ~ dnorm(40, 0.001) 
-    Linf_tau ~ dgamma(0.01, 0.001)
-    k_mu ~ dunif(0.01, 5)
-    k_tau ~ dgamma(0.01, 0.001)
-    tau ~ dgamma(0.01, 0.001)
-    sig ~ dgamma(0.01, 0.001)
-    t0 ~ dunif(-2, 2)
-    }
-    
-    
-    ",fill=TRUE)
+  Linf_mu ~ dnorm(40, 0.001) 
+  Linf_tau ~ dgamma(0.01, 0.001)
+  k_mu ~ dunif(0.01, 5)
+  k_tau ~ dgamma(0.01, 0.001)
+  tau ~ dgamma(0.01, 0.001)
+  sig ~ dgamma(0.01, 0.001)
+  t0 ~ dunif(-2, 2)
+}
+  ", fill = TRUE)
 sink()
 
 #-----------------------
@@ -80,31 +66,27 @@ sink()
 #-----------------------
 sink("PopDyProjAdult.txt")
 cat("
-
 model{
-    for (i in 1:N)     {
-        for (j in 1:n.caps[i]-1)   {    # j indexes recap occasion
-           
-            dL[i,j] ~ dnorm(L_Exp[i,j], sig)   
-            L_Exp[i,j] <-  (Linf - L[i,j]) *(1.0 - exp(-k*(dt[i,j])))
-            L_ob[i,j] ~ dnorm(L[i,j], tau)
-            L[i,j+1] ~ dunif(30, 100)
-
-            # posterior prediction
-            L.pred[i,j] ~ dnorm(L_Exp[i,j], sig)
-            p.value[i,j] <- step(L[i,j] + L.pred[i,j] - L[i,j+1])
-        }
-            L[i,1] ~ dunif(30, 100)
-
+  for (i in 1:N) {              # i indexes individual animal
+    for (j in 1:n.caps[i]-1) {  # j indexes recap occasion
+      dL[i, j] ~ dnorm(L_Exp[i, j], sig)   
+      L_Exp[i, j] <-  (Linf - L[i, j]) * (1.0 - exp(-k * (dt[i, j])))
+      L_ob[i, j] ~ dnorm(L[i, j], tau)
+      L[i, j+1] ~ dunif(30, 100)
+# posterior prediction
+      L.pred[i, j] ~ dnorm(L_Exp[i, j], sig)
+      p.value[i, j] <- step(L[i, j] + L.pred[i, j] - L[i, j+1])
+    }
+    L[i, 1] ~ dunif(30, 100)
   }
+
+# Priors
   Linf ~ dnorm(60, 0.0001)
-    k ~ dunif(0.01, 1)
-    sig ~ dgamma(0.001, 0.0001)
-    tau ~ dgamma(0.001, 0.0001)
+  k ~ dunif(0.01, 1)
+  sig ~ dgamma(0.001, 0.0001)
+  tau ~ dgamma(0.001, 0.0001)
 }
-    
-    
-    ",fill=TRUE)
+  ", fill = TRUE)
 sink()
 
 #--------------------------------------------
@@ -113,29 +95,26 @@ sink()
 sink("PopDyProjAdults.txt")
 cat("
 model{                                           
-    for (i in 1:N)     {                   # i indexes individual animal
-        for (j in 1:n.caps[i]-1)   {    # j indexes recap occasion
-           
-    dL[i,j] ~ dnorm(L_Exp[i,j], sig)   
-    L_Exp[i,j] <-  (Linf[i] - L[i,j]) *(1.0 - exp(-k[i]*(dt[i,j])))
-    L_ob[i,j] ~ dnorm(L[i,j], tau)
-    L[i,j+1] ~ dunif(30, 100)
+  for (i in 1:N) {                   # i indexes individual animal
+    for (j in 1:n.caps[i]-1) {       # j indexes recap occasion
+      dL[i, j] ~ dnorm(L_Exp[i, j], sig)   
+      L_Exp[i, j] <-  (Linf[i] - L[i, j]) * (1.0 - exp(-k[i] * (dt[i, j])))
+      L_ob[i, j] ~ dnorm(L[i, j], tau)
+      L[i,j+1] ~ dunif(30, 100)
     
-    # posterior prediction
-    L.pred[i,j] ~ dnorm(L_Exp[i,j], sig)
-    p.value[i,j] <- step(L[i,j] + L.pred[i,j] - L[i,j+1])
-
-        }
-
-    L[i,1] ~ dunif(30, 100)
+# posterior prediction
+      L.pred[i, j] ~ dnorm(L_Exp[i, j], sig)
+      p.value[i, j] <- step(L[i, j] + L.pred[i, j] - L[i, j+1])
+    }
+    L[i, 1] ~ dunif(30, 100)
     Linf[i] ~ dnorm(Linf_mu,  Linf_tau)     
-    k[i] ~ dnorm(k_mu, k_tau) I(0,1)
-
+    k[i] ~ dnorm(k_mu, k_tau) I(0, 1)
   }
 
-  Linf_std <- sqrt(1/Linf_tau)
-  k_std <- sqrt(1/k_tau)
-  tau_std <- sqrt(1/tau)
+# Priors
+  Linf_std <- sqrt(1 / Linf_tau)
+  k_std <- sqrt(1 / k_tau)
+  tau_std <- sqrt(1 / tau)
     
   Linf_mu ~ dnorm(60, 0.001)
   Linf_tau ~ dgamma(0.01, 0.001)
@@ -144,103 +123,81 @@ model{
   tau ~ dgamma(0.01, 0.001)
   sig ~ dgamma(0.01, 0.001)
 }
-    
-    
-    ",fill=TRUE)
+  ", fill = TRUE)
 sink()
-
 
 #--------------------------------------------
 # Model 5 - Both with heterogeneity
 #--------------------------------------------
 sink("PopDyProjFinal.txt")
 cat("
-    model{                                           
-    for (i in 1:M)     {
-          S[i] ~ dnorm(S_Exp[i], sig1)   
-          S_Exp[i] <-  a1[i]*(1.0 - exp(-k1[i]*(A[i]-t0)))
-          A_ob[i] ~ dnorm(A[i], tau.e)
-          A[i] ~ dunif(0.0001, 0.3)
+model{                                           
+  for (i in 1:M) {
+    S[i] ~ dnorm(S_Exp[i], sig1)   
+    S_Exp[i] <-  a1[i] * (1.0 - exp(-k1[i] * (A[i] - t0)))
+    A_ob[i] ~ dnorm(A[i], tau.e)
+    A[i] ~ dunif(0.0001, 0.3)
 
-          # posterior prediction
-          S.pred[i] ~ dnorm(S_Exp[i], sig1)
-          q.value[i] <- step(S.pred[i] - S[i])
+# posterior prediction
+    S.pred[i] ~ dnorm(S_Exp[i], sig1)
+    q.value[i] <- step(S.pred[i] - S[i])
     
-          a1[i] ~ dnorm(a.mu1,  tau.a)
-          k1[i] ~ dnorm(k.mu1, tau.k)
+    a1[i] ~ dnorm(a.mu1,  tau.a)
+    k1[i] ~ dnorm(k.mu1, tau.k)
+  }
     
-        }
+  for (i in 1:N) {                # i indexes individual animal
+    for (j in 1:n.caps[i]-1) {    # j indexes recap occasion
+      dL[i, j] ~ dnorm(L_Exp[i, j], sig2)   
+      L_Exp[i, j] <-  (a2[i] - L[i, j]) * (1.0 - exp(-k2[i] * (dt[i, j])))
+      L_ob[i, j] ~ dnorm(L[i, j], tau.e)
+      L[i, j+1] ~ dunif(30, 100)
     
-    for (i in 1:N)     {            # i indexes individual animal
-    for (j in 1:n.caps[i]-1)   {    # j indexes recap occasion
-    
-    dL[i,j] ~ dnorm(L_Exp[i,j], sig2)   
-    L_Exp[i,j] <-  (a2[i] - L[i,j]) *(1.0 - exp(-k2[i]*(dt[i,j])))
-    L_ob[i,j] ~ dnorm(L[i,j], tau.e)
-    L[i,j+1] ~ dunif(30, 100)
-    
-    # posterior prediction
-    L.pred[i,j] ~ dnorm(L_Exp[i,j], sig2)
-    p.value[i,j] <- step(L[i,j] + L.pred[i,j] - L[i,j+1])
-    
+# posterior prediction
+      L.pred[i, j] ~ dnorm(L_Exp[i, j], sig2)
+      p.value[i, j] <- step(L[i, j] + L.pred[i, j] - L[i, j+1])
     }
         
-      a2[i] ~ dnorm(a.mu2,  tau.a)   
-      
-      k2[i] ~ dnorm(k.mu2, tau.k)
-      
-      L[i,1] ~ dunif(30, 100)
-      
+    a2[i] ~ dnorm(a.mu2,  tau.a)   
+    k2[i] ~ dnorm(k.mu2, tau.k)
+    L[i, 1] ~ dunif(30, 100)
+  }
     
-    }
-    
-    # Priors
-    
+# Priors
+  a.mu1 ~ dnorm(40, 0.2)
+  a.mu2 ~ dnorm(60, 0.001) 
+  s.a ~ dunif(0, 100) 
+  k.mu1 ~ dnorm(0, 0.000001)
+  k.mu2 ~ dnorm(0, 0.000001)
+  s.k ~ dunif(0, 100) 
+  s.e ~ dunif(0, 100) 
+  sig1 ~ dgamma(0.1, 0.1) 
+  sig2 ~ dgamma(0.1, 0.1) 
+  
+  tau.a <- 1/pow(s.a,2)
+	tau.k <- 1/pow(s.k,2)
+	tau.e <- 1/pow(s.e,2)
 
-    
-    a.mu1 ~ dnorm(40, 0.2)
-    a.mu2 ~ dnorm(60, 0.001) 
-    
-    s.a ~ dunif(0, 100) 
-    
-    k.mu1 ~ dnorm(0, 0.000001)
-    k.mu2 ~ dnorm(0, 0.000001)
-    
-    s.k ~ dunif(0, 100) 
-    
-    s.e ~ dunif(0, 100) 
-    
-    sig1 ~ dgamma(0.1, 0.1) 
-    sig2 ~ dgamma(0.1, 0.1) 
-    
-    tau.a <- 1/pow(s.a,2)		# next 3 lines convert sd to precision
-	  tau.k <- 1/pow(s.k,2)
-	  tau.e <- 1/pow(s.e,2)
-    
-    t0 ~ dunif(-2, 2)
-    tmax ~ dunif(0.25, 0.5)
-    }
-    
-    ",fill=TRUE)
+  t0 ~ dunif(-2, 2)
+  tmax ~ dunif(0.25, 0.5)
+}
+  ", fill = TRUE)
 sink()
-
-
 
 #----------------------------------------------
 # Model 6 - Both with heterogeneity and linked
 #----------------------------------------------
 sink("PopDyProjFinal.txt")
 cat("
-    model{                                           
-    for (i in 1:L.n)     {                              # larval growth (known ages)
-
-    LL[i] <-  Linf.mu1 * (1-exp(-k1[i]*(t[i]-t0)))    # objective function
-    LL.ob[i] ~ dnorm(LL[i], tau.L)I(0,)                    # likelihood function 
-    t.ob[i] ~ dnorm(t[i], tau.t)I(t.ob[i],0.5)                      # likelihood function
+model{                                           
+  for (i in 1:L.n) {  # larval growth (known ages)
+    LL[i] <-  Linf.mu1 * (1 - exp(-k1[i] * (t[i] - t0)))    # objective function
+    LL.ob[i] ~ dnorm(LL[i], tau.L) I(0, )                   # likelihood function 
     
-        t[i] ~ dunif(0.01, t.m)                      # prior (with hyper parameters)
+    t.ob[i] ~ dnorm(t[i], tau.t) I(0.001, t.ob[i])          # likelihood function
+    t[i] ~ dunif(0.01, t.m)                                 # prior (with hyper parameters)
         
-        k1[i] ~ dnorm(k.mu1, k.tau1)I(2,4)                  # prior (with hyper parameters)
+    k1[i] ~ dnorm(k.mu1, k.tau1) I(2, 4)                    # prior (with hyper parameters)
 
     # posterior prediction
     L.pred[i] ~ dnorm(LL[i], tau.L)I(0,)         
@@ -249,80 +206,60 @@ cat("
     
   
   }
-    L.m <- Linf.mu1 * (1-exp(-k.mu1*(t.m-t0)))
   
-    for (i in 1:N)     {                              # adult growth (unknown ages)
-      for (j in 1:n.caps[i]-1)   {                    # j indexes recap occasion
-    
-      dL[i,j] <-  (Linf2[i] - L[i,j]) *(1.0 - exp(-k2[i]*(dt[i,j])))   # objective function   
-      dL.ob[i,j] ~ dnorm(dL[i,j], tau.a)                               # likelihood function
-      L.ob[i,j] ~ dnorm(L[i,j], tau.L)I(0,)                                 # likelihood function
+  L.m <- Linf.mu1 * (1 - exp(-k.mu1 * (t.m - t0)))
+  
+    for (i in 1:N) {                                                           # adult growth (unknown ages)
+      for (j in 1:n.caps[i]-1) {                                               # j indexes recap occasion
+        dL[i, j] <-  (Linf2[i] - L[i, j]) * (1.0 - exp(-k2[i] * (dt[i, j])))   # objective function   
+        dL.ob[i, j] ~ dnorm(dL[i, j], tau.a)                                   # likelihood function
+        L.ob[i, j] ~ dnorm(L[i, j], tau.L) I(0, )                              # likelihood function
 
-        L[i,j+1] ~ dunif(40, 70)                                       # prior
+        L[i, j+1] ~ dunif(40, 70)                                              # prior
 
 
-    # posterior prediction
-    dL.pred[i,j] ~ dnorm(dL[i,j], tau.a)
-    p.value[i,j] <- step(L[i,j] + dL.pred[i,j] - L[i,j+1])
-
-    
-      }
+# posterior prediction
+        dL.pred[i, j] ~ dnorm(dL[i, j], tau.a)
+        p.value[i, j] <- step(L[i, j] + dL.pred[i, j] - L[i, j+1])
+      }  
            
-            L[i,1] <- (Linf2[i] - L.m) *(1.0 - exp(-k2[i]*(t.a[i]-t.m))) + L.m
-            
-            t.a[i] ~ dlnorm(1,0.5)I(t.m,12)
+      L[i,1] <- (Linf2[i] - L.m) * (1.0 - exp(-k2[i] * (t.a[i] - t.m))) + L.m
+      t.a[i] ~ dlnorm(1, 0.5) I(t.m, 12)
 
-            t1[i] <- t.m - log((Linf2[i]-L.m)/Linf2[i])/-k2[i]
+      t1[i] <- t.m - log((Linf2[i] - L.m) / Linf2[i]) / -k2[i]
 
-            Linf2[i] ~ dnorm(Linf.mu2,  Linf.tau2)I(0,)              # prior (with hyperparameters)
-            k2[i] ~ dnorm(k.mu2, k.tau2)I(0,2)                        # prior (with hyperparameters)
-  }    
+      Linf2[i] ~ dnorm(Linf.mu2,  Linf.tau2) I(0, )               # prior (with hyperparameters)
+      k2[i] ~ dnorm(k.mu2, k.tau2) I(0, 2)                        # prior (with hyperparameters)
+    }      
 
-
-  # Priors
+# Priors
   
-    Linf.std2 <- sqrt(1/Linf.tau2)
+  Linf.std2 <- sqrt(1 / Linf.tau2)
+  k.std1 <- sqrt(1 / k.tau1)
+  k.std2 <- sqrt(1 / k.tau2)
     
-    k.std1 <- sqrt(1/k.tau1)
-    k.std2 <- sqrt(1/k.tau2)
-    
-    L.std <- sqrt(1/tau.L)   
-    a.std <- sqrt(1/tau.a)   
-    t.std <- sqrt(1/tau.t)   
+  L.std <- sqrt(1 / tau.L)   
+  a.std <- sqrt(1 / tau.a)   
+  t.std <- sqrt(1 / tau.t)   
 
-    Linf.mu1 ~ dunif(30, 70)
-    Linf.mu2 ~ dunif(50, 70)
+  Linf.mu1 ~ dunif(30, 70)
+  Linf.mu2 ~ dunif(50, 70)
 
+  Linf.tau2 ~ dgamma(0.01, 0.01) 
+    
+  k.mu1 ~ dunif(2, 4)
+  k.mu2 ~ dlnorm(0, 10) I(0, 5) 
 
-    Linf.tau2 ~ dgamma(0.01, 0.01) 
+  k.tau1 ~ dgamma(0.01, 0.01) 
+  k.tau2 ~ dgamma(0.01, 0.01)
     
-    k.mu1 ~ dunif(2, 4)
-    k.mu2 ~ dlnorm(0, 10)I(0,1.5) 
-
-
-    k.tau1 ~ dgamma(0.01, 0.01) 
-    k.tau2 ~ dgamma(0.01, 0.01)
+  tau.L ~ dgamma(0.01, 0.01) 
+  tau.a ~ dgamma(0.01, 0.01) 
+  tau.t ~ dgamma(0.01, 0.01) 
     
-    tau.L ~ dgamma(0.01, 0.01) 
-    tau.a ~ dgamma(0.01, 0.01) 
-    tau.t ~ dgamma(0.01, 0.01) 
-    
-    t0 ~ dunif(-2, 2)
-    t.m ~ dunif(0.3, 0.5)
-    t1.mu <- mean(t1[])
-  }
-    
-    ",fill=TRUE)
+  t0 ~ dunif(-2, 2)
+  t.m ~ dunif(0.3, 0.5)
+  t1.mu <- mean(t1[])
+}
+  ", fill = TRUE)
 sink()
-
-
-
-#  L.m <- mean(Linf1[])*(1.0 - exp(mean(-k1[])*(t.m-t0)))
-# t.m[i] <- log((L.m[i]-Linf1[i])/Linf1[i])/k1[i] + t0
-#   t.m ~ dunif(0.2, 0.4)
-
-
-# L.a <- Linf.mu2*(1.0 - exp(-k.mu2*(t.m-t0)))
-
-#  L.meta <- mean(L.m[]) - mean(L.a[])
-# L.meta ~ dnorm(L.m, tau.L)
--- PopDy_Project_Run.R
+++ PopDy_Project_Run.R
@@ -1,6 +1,8 @@
 rm(list = ls())
 library(R2WinBUGS)
-setwd("C:/Users/boa10gb/Documents/R/VB Growth")
+library(ggplot2)
+library(plyr)
+library(reshape)
 
 #------------
 # Model 1
@@ -11,29 +13,23 @@ df = read.csv("larvae_SVL.csv", sep = ",")
 A_ob = df$Age
 L = df$SVL
 dat = na.omit(cbind(A_ob, L, df$Group))
-dat[which(dat[,3]==1),1] = dat[which(dat[,3]==1),1]-25  
-
 
 bugs.data = list(
-  L = dat[,2],
-  A_ob = dat[,1]/365,
-  N = length(dat[,1])
+  L = dat[, 2],
+  A_ob = dat[, 1] / 365,
+  N = length(dat[, 1])
 )
 
-# Defining age, maybe push back to October 15???
-library(ggplot2)
-
 dat1 = data.frame(dat)
-ggplot(dat1, aes(x = A_ob/365, y = L))+
-  geom_point(aes(colour = as.factor(dat1[,3])))
+ggplot(dat1, aes(x = A_ob / 365, y = L)) +
+  geom_point(aes(colour = as.factor(dat1[, 3])))
 
 # Initial values
 #inits <- function(){list(Linf = 90, k = 0.5, tau.eps = 10, m.eps = 10)}  
-inits <- function(){list(Linf = runif(411, 39, 41), k = runif(411, 1,1.2), sig = 5, tau = 5, t0 = 0)}  
+inits <- function(){list(Linf = runif(411, 39, 41), k = runif(411, 1, 1.2), sig = 5, tau = 5, t0 = 0)}  
 
 # Parameters monitored
-# parameters <- c("k", "Linf", "tau.eps", "m.eps")
-parameters <- c("Linf_mu","Linf_std", "k_mu","k_std", "tau_std", "t0")
+parameters <- c("Linf_mu", "Linf_std", "k_mu", "k_std", "tau_std", "t0")
 
 # MCMC settings
 ni <- 100000
@@ -45,33 +41,41 @@ nc <- 3
 js.super <- bugs(bugs.data, inits, parameters, "PopDyProjLarvae.txt", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb,
                  debug = TRUE, bugs.directory = "C:/Program Files (x86)/WinBUGS14", working.directory = getwd())
 
-
 #-----------------------------------------------------------------------
 # Posterior Plots
-library(ggplot2)
-library(plyr)
-library(reshape)
 pl = data.frame(js.super$sims.list)
-
 
 pl = pl[-7]
 colnames(pl) = c("Linf", "Linf Sigma", "k", "k Sigma", "Tau", "t0")
 
 dat.melt <- melt(pl)
 
-ggplot()+
-  geom_density(data = dat.melt, aes(value, 
-                                    fill = variable, colour = variable), 
-               alpha=I(.1), size =1)  +
+ggplot() +
+  geom_density(data = dat.melt, aes(value, fill = variable, colour = variable), 
+               alpha = I(.1), size = 1) +
   facet_wrap(~ variable, scales = "free", nrow = 3)
 
 
+k.l = density(pl[,1])
+k.a = density(pl[,3])
+
+plot(k.l, ylim = c(0,10), xlim = c(0, 4), lwd = 2, lty = 1, 
+     ylab = "", xlab = "", main = "Posterior Distributions k", axes = FALSE)
+
+par(new = TRUE)
+
+plot(k.a, ylim = c(0, 10), xlim = c(0, 4), lwd = 2, lty = 2, ylab = "", xlab = "", main = "", axes = FALSE)
+axis(1)
+axis(2)
+par(las = 0)
+mtext("k", side = 1, line = 2.5, cex = 1.5)
+mtext("Density", side = 2, line = 3, cex = 1.8)
 
 # VBG curve plot
 k.pred.l <- js.super$mean$k_mu
 Linf.pred.l <- js.super$mean$Linf_mu
 
-plot(curve(Linf.pred.l*(1-exp(-k.pred.l*x)), 0, 10), pch = 16, cex = 0.5, ylim = c(0, 70),
+plot(curve(Linf.pred.l * (1 - exp(-k.pred.l * x)), 0, 10), pch = 16, cex = 0.5, ylim = c(0, 70),
      ylab = "", xlab = "", main = "VB Growth", axes = FALSE)
 axis(1)
 axis(2)
@@ -79,20 +83,11 @@ par(las = 0)
 mtext("Age", side = 1, line = 2.5, cex = 1.5)
 mtext("Length", side = 2, line = 3, cex = 1.8)
 
-
-
-
 #-------------
 # Model 2
 #-----------
-rm(list = ls())
-library(R2WinBUGS)
-setwd("C:/Users/boa10gb/Documents/R/VB Growth")
-
-
 df = read.csv("FW_main.csv", sep = ",")
-
-df = df[,c("MASTER.ID", "Date", "Sex", "Weight", "TL", "SVL_1stMeasurement")]
+df = df[, c("MASTER.ID", "Date", "Sex", "Weight", "TL", "SVL_1stMeasurement")]
 
 dl = split(df, df$MASTER.ID, drop = T)
 names(dl) = NULL
@@ -112,38 +107,30 @@ count = 0
 n.caps = sapply(dl, NROW)
 
 # for each unique ID, get dt, dL, and L
-for(i in 1:length(dl)){
-
-  if(n.caps[i] == 1){
-    
-  }
-  
-  else{
+for (i in 1:length(dl)) {
+  if (n.caps[i] == 1) {
+  } else {
     count = count + 1
-    for(j in 2:length(dl[[i]][[1]])){
+      for (j in 2:length(dl[[i]][[1]])) {
       
-      Wt[count,j-1] = dl[[i]][j-1,4]
-      Wt[count,j] = dl[[i]][j,4]
+        Wt[count, j-1] = dl[[i]][j-1, 4]
+        Wt[count, j] = dl[[i]][j, 4]
       
-      TLt[count,j-1] = dl[[i]][j-1,5]
-      TLt[count,j] = dl[[i]][j,5]
+        TLt[count, j-1] = dl[[i]][j-1, 5]
+        TLt[count, j] = dl[[i]][j, 5]
       
-      SVLt[count,j-1] = dl[[i]][j-1,6]
-      SVLt[count,j] = dl[[i]][j,6]
+        SVLt[count, j-1] = dl[[i]][j-1, 6]
+        SVLt[count, j] = dl[[i]][j, 6]
       
-      dW[count,j-1] = dl[[i]][j,4] - dl[[i]][j-1,4]
-      dTL[count,j-1] = dl[[i]][j,5] - dl[[i]][j-1,5]
-      dSVL[count,j-1] = dl[[i]][j,6] - dl[[i]][j-1,6]
+        dW[count, j-1] = dl[[i]][j, 4] - dl[[i]][j-1, 4]
+        dTL[count, j-1] = dl[[i]][j, 5] - dl[[i]][j-1, 5]
+        dSVL[count, j-1] = dl[[i]][j, 6] - dl[[i]][j-1, 6]
       
-      dt[count,j-1] = as.Date(dl[[i]][j,2], format = "%m/%d/%Y") - as.Date(dl[[i]][j-1,2], format = "%m/%d/%Y")
-      
-          
-         
+        dt[count, j-1] = as.Date(dl[[i]][j, 2], format = "%m/%d/%Y") - as.Date(dl[[i]][j-1, 2], format = "%m/%d/%Y")
       } # j
-    ID = append(ID, dl[[i]][1,1])
-    n = append(n, n.caps[i])
+      ID = append(ID, dl[[i]][1, 1])
+      n = append(n, n.caps[i])
   } 
-
 }   # i
 
 
@@ -152,17 +139,16 @@ bugs.data = list(
   n.caps = n,
   L_ob = SVLt,
   dL = dSVL,
-  dt = dt/365,
+  dt = dt / 365,
   N = length(n)
 )
 
 
 # Initial values
-inits <- function(){list(Linf = runif(353, 59,61), k = runif(353 , 0.4, 0.6), 
-     tau =  5, sig = 5)}
+inits <- function(){list(Linf = runif(353, 59, 61), k = runif(353, 0.4, 0.6), 
+     tau = 5, sig = 5)}
      
-parameters <- c("Linf_mu","Linf_std", "k_mu","k_std", "tau_std")
-
+parameters <- c("Linf_mu", "Linf_std", "k_mu", "k_std", "tau_std")
 
 # MCMC settings
 ni <- 100000
@@ -174,11 +160,7 @@ nc <- 3
 js.super <- bugs(bugs.data, inits, parameters, "PopDyProjAdults.txt", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb,
                  debug = TRUE, bugs.directory = "C:/Program Files (x86)/WinBUGS14", working.directory = getwd())
 
-
 # Posterior Plots 
-library(ggplot2)
-library(plyr)
-library(reshape)
 
 pl = data.frame(js.super$sims.list)
 pl = pl[-6]
@@ -186,21 +168,19 @@ colnames(pl) = c("Linf", "Linf Sigma", "k", "k Sigma", "Tau")
 
 dat.melt <- melt(pl)
 
-ggplot()+
-  geom_density(data = dat.melt, aes(value, 
-                                    fill = variable, colour = variable), 
-               alpha=I(.1), size =1)  +
+ggplot() +
+  geom_density(data = dat.melt, aes(value, fill = variable, colour = variable), 
+               alpha = I(.1), size = 1) +
   facet_wrap(~ variable, scales = "free", nrow = 3)
-
 
 # VBG curve plot
 k.pred.a <- js.super$mean$k_mu
 Linf.pred.a <- js.super$mean$Linf_mu
 
-plot(curve(Linf.pred.a*(1-exp(-k.pred.a*x)), 0, 10), pch = 16, cex = 0.5, ylim = c(0, 70),
+plot(curve(Linf.pred.a * (1 - exp(-k.pred.a * x)), 0, 10), pch = 16, cex = 0.5, ylim = c(0, 70),
      ylab = "", xlab = "", main = "VB Growth", axes = FALSE)
 
-f = function(x){Linf.pred.l*(1-exp(-k.pred.l*x))}
+f = function(x){Linf.pred.l * (1 - exp(-k.pred.l * x))}
 points(seq(from = 0, to = 10, length.out = 100), f(seq(from = 0, to = 10, length.out = 100)), pch = 17, cex = 0.5)
 axis(1)
 axis(2)
@@ -209,17 +189,9 @@ mtext("Age", side = 1, line = 2.5, cex = 1.5)
 mtext("Length", side = 2, line = 3, cex = 1.8)
 legend(6, 30, legend = c("Adult", "Larvae"), pch = c(16, 17))
 
-
-
-
-
 #-----------------------------------------------------------------------
 # Model 5 -Full Model
 #-----------------------------------------------------------------------
-
-rm(list = ls())
-library(R2WinBUGS)
-setwd("C:/Users/boa10gb/Documents/R/VB Growth")
 
 #------------
 # Model 1
@@ -230,17 +202,10 @@ df = read.csv("larvae_SVL.csv", sep = ",")
 A_ob = df$Age
 S = df$SVL
 dat = na.omit(cbind(A_ob, S, df$Group))
-dat[which(dat[,3]==1),1] = dat[which(dat[,3]==1),1]-25  
-
-
-
-
-
-
+dat[which(dat[, 3] == 1), 1] = dat[which(dat[, 3] == 1), 1] - 25  
 
 df = read.csv("FW_main.csv", sep = ",")
-
-df = df[,c("MASTER.ID", "Date", "Sex", "Weight", "TL", "SVL_1stMeasurement")]
+df = df[, c("MASTER.ID", "Date", "Sex", "Weight", "TL", "SVL_1stMeasurement")]
 
 dl = split(df, df$MASTER.ID, drop = T)
 names(dl) = NULL
@@ -260,15 +225,11 @@ count = 0
 n.caps = sapply(dl, NROW)
 
 # for each unique ID, get dt, dL, and L
-for(i in 1:length(dl)){
-  
-  if(n.caps[i] == 1){
-    
-  }
-  
-  else{
-    count = count + 1
-    for(j in 2:length(dl[[i]][[1]])){
+for (i in 1:length(dl)) {
+  if (n.caps[i] == 1) {
+  } else {
+      count = count + 1
+        for (j in 2:length(dl[[i]][[1]])) {
       
       Wt[count,j-1] = dl[[i]][j-1,4]
       Wt[count,j] = dl[[i]][j,4]
@@ -343,16 +304,16 @@ parameters <- c( "Linf.mu1", "Linf.mu2", "Linf.std2", "k.mu1", "k.std1", "k.mu2"
                  "t", "L.pred", "q.value", "L", "dL.pred", "p.value", "t.a", "t1")
  
 # MCMC settings
-ni <- 1000
-nt <- 20
-nb <- 10
+ni <- 1000000
+nt <- 500
+nb <- 10000
 nc <- 1
 
 # Call WinBUGS from R (BRT 40 min)
 js.super <- bugs(bugs.data, inits, parameters, "PopDyProjFinal.txt", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb,
                  debug = TRUE, bugs.directory = "C:/Program Files (x86)/WinBUGS14", working.directory = getwd())
 
-save(js.super, file = "outfin9.RData")
+save(js.super, file = "outfin10.RData")
 
 plot(js.super)
 
@@ -369,7 +330,7 @@ pl = data.frame(js.super$sims.list)
 pl = pl[(1:14)]
 colnames(pl) = c("Linf.mu1", "Linf.mu2", "Linf.std2", "k.mu1", "k.std1", "k.mu2", "k.std2",
                  "t.m", "L.m", "L.std", "a.std", "t.std", "t0", "t1.mu")
-pp = pl[c(4,6]
+pp = pl[c(4,6)]
 
 k.l = density(pp[,1], )
 k.a = density(pp[,2])
